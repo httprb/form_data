56884332f9eca701c2131e3a0cf485629baf8531
Janko Marohnić <janko.marohnic@gmail.com>
2017-05-01 22:53:54 +1000

    Make form data streamable
    This is a squashed commit of [PR#12][] with some tiny cleanups
    applied on top of that.
    
    [PR#12]: https://github.com/httprb/form_data.rb/pull/12
    
    Allow any IO object in FormData::File
    -------------------------------------
    
    Previously we allowed only File and StringIO objects as an input
    to `FormData::File`, but we can generalize that to any IO object that
    responds to `#read` and `#size` (which includes `Tempfile`,
    `ActionDispatch::Http::UploadedFile` etc).
    
    Open File for given path in binary mode
    ---------------------------------------
    
    That way different operating systems won't attempt to convert newline
    characters to their internal representation, instead the file content
    will always be retrieved byte-for-byte as is.
    
    Officially support Pathname in FormData::File.new
    -------------------------------------------------
    
    Previously Pathname was implicitly supported, though extracting
    filename wasn't working. With the recent refactoring this stopped
    working, so we make the Pathname support explicit.
    
    Make all components into IO objects
    -----------------------------------
    
    By changing all components to use an IO object as a base, we can
    implement a common IO interface for all components, which delegates to
    the underlying IO object.
    
    This enables streaming multipart data into the request body, avoiding
    loading the whole multipart data into memory when File parts are backed
    by File objects.
    
    See httprb/http#409 for the new streaming API.
    
    Make CompositeIO convert strings to StringIOs
    ---------------------------------------------
    
    By delegating handling strings to CompositeIO we can remove a lot of the
    StringIO.new clutter when instantiating CompositeIO objects.
    
    Use a buffer when reading IO files in CompositeIO
    -------------------------------------------------
    
    This way we're not creating a new string for each chunk read, instead
    each chunk will be read into an existing string object (a "buffer"),
    replacing any previous content.

--- lib/http/form_data/part.rb
+++ lib/http/form_data/part.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require "stringio"
+
+require "http/form_data/readable"
+
 module HTTP
   module FormData
     # Represents a body part of multipart/form-data request.
@@ -9,29 +13,17 @@ module HTTP
     #  body = "Message"
     #  FormData::Part.new body, :content_type => 'foobar.txt; charset="UTF-8"'
     class Part
+      include Readable
+
       attr_reader :content_type, :filename
 
       # @param [#to_s] body
       # @param [String] content_type Value of Content-Type header
       # @param [String] filename     Value of filename parameter
       def initialize(body, content_type: nil, filename: nil)
-        @body = body.to_s
+        @io = StringIO.new(body.to_s)
         @content_type = content_type
         @filename = filename
-      end
-
-      # Returns content size.
-      #
-      # @return [Integer]
-      def size
-        @body.bytesize
-      end
-
-      # Returns content of a file of IO.
-      #
-      # @return [String]
-      def to_s
-        @body
       end
     end
   end
--- spec/lib/http/form_data/file_spec.rb
+++ spec/lib/http/form_data/file_spec.rb
@@ -12,15 +12,20 @@ RSpec.describe HTTP::FormData::File do
       it { is_expected.to eq fixture("the-http-gem.info").size }
     end
 
-    context "when file given as StringIO" do
-      let(:file) { StringIO.new "привет мир!" }
-      it { is_expected.to eq 20 }
+    context "when file given as a Pathname" do
+      let(:file) { fixture("the-http-gem.info") }
+      it { is_expected.to eq fixture("the-http-gem.info").size }
     end
 
     context "when file given as File" do
       let(:file) { fixture("the-http-gem.info").open }
       after { file.close }
       it { is_expected.to eq fixture("the-http-gem.info").size }
+    end
+
+    context "when file given as IO" do
+      let(:file) { StringIO.new "привет мир!" }
+      it { is_expected.to eq 20 }
     end
   end
 
@@ -32,15 +37,90 @@ RSpec.describe HTTP::FormData::File do
       it { is_expected.to eq fixture("the-http-gem.info").read(:mode => "rb") }
     end
 
-    context "when file given as StringIO" do
-      let(:file) { StringIO.new "привет мир!" }
-      it { is_expected.to eq "привет мир!" }
+    context "when file given as a Pathname" do
+      let(:file) { fixture("the-http-gem.info") }
+      it { is_expected.to eq fixture("the-http-gem.info").read(:mode => "rb") }
     end
 
     context "when file given as File" do
       let(:file) { fixture("the-http-gem.info").open("rb") }
       after { file.close }
       it { is_expected.to eq fixture("the-http-gem.info").read(:mode => "rb") }
+    end
+
+    context "when file given as IO" do
+      let(:file) { StringIO.new "привет мир!" }
+      it { is_expected.to eq "привет мир!" }
+    end
+  end
+
+  describe "#read" do
+    subject { described_class.new(file, opts).read }
+
+    context "when file given as a String" do
+      let(:file) { fixture("the-http-gem.info").to_s }
+      it { is_expected.to eq fixture("the-http-gem.info").read(:mode => "rb") }
+    end
+
+    context "when file given as a Pathname" do
+      let(:file) { fixture("the-http-gem.info") }
+      it { is_expected.to eq fixture("the-http-gem.info").read(:mode => "rb") }
+    end
+
+    context "when file given as File" do
+      let(:file) { fixture("the-http-gem.info").open("rb") }
+      after { file.close }
+      it { is_expected.to eq fixture("the-http-gem.info").read(:mode => "rb") }
+    end
+
+    context "when file given as IO" do
+      let(:file) { StringIO.new "привет мир!" }
+      it { is_expected.to eq "привет мир!" }
+    end
+  end
+
+  describe "#rewind" do
+    subject { described_class.new(file, opts) }
+
+    context "when file given as a String" do
+      let(:file) { fixture("the-http-gem.info").to_s }
+
+      it "rewinds the underlying IO object" do
+        content = subject.read
+        subject.rewind
+        expect(subject.read).to eq content
+      end
+    end
+
+    context "when file given as a Pathname" do
+      let(:file) { fixture("the-http-gem.info") }
+
+      it "rewinds the underlying IO object" do
+        content = subject.read
+        subject.rewind
+        expect(subject.read).to eq content
+      end
+    end
+
+    context "when file given as File" do
+      let(:file) { fixture("the-http-gem.info").open("rb") }
+      after { file.close }
+
+      it "rewinds the underlying IO object" do
+        content = subject.read
+        subject.rewind
+        expect(subject.read).to eq content
+      end
+    end
+
+    context "when file given as IO" do
+      let(:file) { StringIO.new "привет мир!" }
+
+      it "rewinds the underlying IO object" do
+        content = subject.read
+        subject.rewind
+        expect(subject.read).to eq content
+      end
     end
   end
 
@@ -58,10 +138,10 @@ RSpec.describe HTTP::FormData::File do
       end
     end
 
-    context "when file given as StringIO" do
-      let(:file) { StringIO.new }
+    context "when file given as a Pathname" do
+      let(:file) { fixture("the-http-gem.info") }
 
-      it { is_expected.to eq "stream-#{file.object_id}" }
+      it { is_expected.to eq ::File.basename file }
 
       context "and filename given with options" do
         let(:opts) { { :filename => "foobar.txt" } }
@@ -74,6 +154,17 @@ RSpec.describe HTTP::FormData::File do
       after { file.close }
 
       it { is_expected.to eq "the-http-gem.info" }
+
+      context "and filename given with options" do
+        let(:opts) { { :filename => "foobar.txt" } }
+        it { is_expected.to eq "foobar.txt" }
+      end
+    end
+
+    context "when file given as IO" do
+      let(:file) { StringIO.new }
+
+      it { is_expected.to eq "stream-#{file.object_id}" }
 
       context "and filename given with options" do
         let(:opts) { { :filename => "foobar.txt" } }
