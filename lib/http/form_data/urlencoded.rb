e049b021c8c944b2cf4afc05f427692a6b0f3b6e
Janko Marohnić <janko.marohnic@gmail.com>
2017-05-11 11:40:45 +1000

    Make Urlencoded component into an IO object
    Following recent transformations of other components into IO objects:
    
    * FormData::Multipart
    * FormData::Multipart::Param
    * FormData::Part
    * FormData::File

--- lib/http/form_data/urlencoded.rb
+++ lib/http/form_data/urlencoded.rb
@@ -1,21 +1,20 @@
 # frozen_string_literal: true
 
+require "http/form_data/readable"
+
 require "uri"
+require "stringio"
 
 module HTTP
   module FormData
     # `application/x-www-form-urlencoded` form data.
     class Urlencoded
+      include Readable
+
       # @param [#to_h, Hash] data form data key-value Hash
       def initialize(data)
-        @data = FormData.ensure_hash data
-      end
-
-      # Returns content to be used for HTTP request body.
-      #
-      # @return [String]
-      def to_s
-        ::URI.encode_www_form @data
+        uri_encoded_data = ::URI.encode_www_form FormData.ensure_hash(data)
+        @io = StringIO.new(uri_encoded_data)
       end
 
       # Returns MIME type to be used for HTTP request `Content-Type` header.
@@ -29,9 +28,7 @@ module HTTP
       # `Content-Length` header.
       #
       # @return [Integer]
-      def content_length
-        to_s.bytesize
-      end
+      alias content_length size
     end
   end
 end
