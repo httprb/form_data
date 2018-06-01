27cc40c33a208296e57bf02f2d85549e5035c1d6
Alexey Zapparov <ixti@member.fsf.org>
2018-06-01 15:59:09 +0200

    Add API docs for Urlencoded.encoder

--- lib/http/form_data/urlencoded.rb
+++ lib/http/form_data/urlencoded.rb
@@ -12,8 +12,51 @@ module HTTP
       include Readable
 
       class << self
-        attr_writer :encoder
+        # Set custom form data encoder implementation.
+        #
+        # @example
+        #
+        #     module CustomFormDataEncoder
+        #       UNESCAPED_CHARS = /[^a-z0-9\-\.\_\~]/i
+        #
+        #       def self.escape(s)
+        #         ::URI::DEFAULT_PARSER.escape(s.to_s, UNESCAPED_CHARS)
+        #       end
+        #
+        #       def self.call(data)
+        #         parts = []
+        #
+        #         data.each do |k, v|
+        #           k = escape(k)
+        #
+        #           if v.nil?
+        #             parts << k
+        #           elsif v.respond_to?(:to_ary)
+        #             v.to_ary.each { |vv| parts << "#{k}=#{escape vv}" }
+        #           else
+        #             parts << "#{k}=#{escape v}"
+        #           end
+        #         end
+        #
+        #         parts.join("&")
+        #       end
+        #     end
+        #
+        #     HTTP::FormData::Urlencoded.encoder = CustomFormDataEncoder
+        #
+        # @raise [ArgumentError] if implementation deos not responds to `#call`.
+        # @param implementation [#call]
+        # @return [void]
+        def encoder=(implementation)
+          raise ArgumentError unless implementation.respond_to? :call
+          @encoder = implementation
+        end
 
+        # Returns form data encoder implementation.
+        # Default: `URI.encode_www_form`.
+        #
+        # @see .encoder=
+        # @return [#call]
         def encoder
           @encoder ||= ::URI.method(:encode_www_form)
         end
